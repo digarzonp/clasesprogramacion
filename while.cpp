10acc9720c909b18a87d960f1e917dbc0be54e70
daniela irene garzon pinto <digarzonp@unal.edu.co>
2018-03-29 07:52:11 -0500

    ejemplo de usar  while

---
+++ while.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout<<"enter the starnting number>";
+  cin>>n;
+
+  while (n>0) {
+    cout <<n<<",";
+    --n;
+}
+  cout <<"fire!\n";
+  return 0;
+}
